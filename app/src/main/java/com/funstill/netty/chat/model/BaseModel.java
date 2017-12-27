abc9a303775b9ffc24afb90ea85e7e4e2540fd51
kelefun <kelefun@foxmail.com>
2017-12-27 09:58:18 +0800

    update

---
+++ app/src/main/java/com/funstill/netty/chat/model/BaseModel.java
@@ -0,0 +1,18 @@
+package com.funstill.netty.chat.model;
+
+/**
+ * Created by liukaiyang on 2017/12/21.
+ */
+
+public class BaseModel {
+    private String id;
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
